a31b3ae5b7eaafb64761172710d0b6adc19bf074
k_dozee <kaustubh@dozee.io>
2023-09-29 00:17:06 +0100

    Added few icons to the dock

--- src/components/Dock.js
+++ src/components/Dock.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./dock.css";
+
 function Dock({ screenBrightnessLevel }) {
   return (
     <div className='dock' style={{ opacity: { screenBrightnessLevel } }}>
@@ -7,6 +8,15 @@ function Dock({ screenBrightnessLevel }) {
         className='dock__apps flex flex-col w-8/12 mx-2 items-center'
         style={{ opacity: { screenBrightnessLevel } }}>
         <div className='chrome-img-dock h-9 w-9 my-3 cursor-pointer opacity-90 hover:opacity-100' />
+        <div className='gedit-img-dock h-10 w-9 my-3 cursor-pointer opacity-90 hover:opacity-100' />
+
+        <img
+          width='48'
+          height='48'
+          className='my-1 cursor-pointer opacity-90 hover:opacity-100'
+          src='https://img.icons8.com/color/48/folder-invoices--v1.png'
+          alt='folder-invoices--v1'
+        />
         <div className='chrome-img-dock h-9 w-9 my-3 cursor-pointer opacity-90 hover:opacity-100' />
         <div className='chrome-img-dock h-9 w-9 my-3 cursor-pointer opacity-90 hover:opacity-100' />
       </div>
